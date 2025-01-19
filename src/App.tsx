251f2dce9cd668cc0f3812f72e78e55aef70546a
tomasmaranga <tomas.maranga@icloud.com>
2025-01-19 14:42:20 -0500

    small change

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-3xl font-bold text-blue-500">
-        Professor Insight Extension 🚀
+        Rate My Professor Extension
       </h1>
     </div>
   );
