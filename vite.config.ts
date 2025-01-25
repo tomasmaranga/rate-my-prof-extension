a10b51260ceba5f8e357bddcbdc33b733bdbe8dd
tomasmaranga <tomas.maranga@icloud.com>
2025-01-25 02:30:49 -0500

    added icons, option to disable

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "src/assets/icons/*",
+          dest: ".",
+        },
+      ],
+    }),
+  ],
   build: {
     outDir: "dist",
     rollupOptions: {
